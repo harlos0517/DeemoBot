9bcabf97428723c6bcb8896c7956b2ee6483dd1f
root <root@student-29.pve.seadog007.co>
2021-08-16 20:09:09 +0800

    add text2score

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import discord as dc
-from discord.ext import commands
-
 import os
 from dotenv import load_dotenv
 
-# import text2score as t2s_m
+import discord as dc
+from discord.ext import commands as cmds
+from discord.ext.commands import Context as Ctx
+
+import text2score as t2s_m
 # import dict_game
 # import popcat
 # import decision
@@ -13,29 +14,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = dc.Client()
-bot = commands.Bot(command_prefix=['!D ', '!Deemo '])
+bot = cmds.Bot(command_prefix=['!D ', '!Deemo '])
+
 
 @bot.event
 async def on_ready():
     print("機器人 Start", bot.user)
+
 
 @bot.event
 async def on_message(message: dc.Message):
     if message.author.bot: return
     await bot.process_commands(message)
     # https://stackoverflow.com/questions/49331096/why-does-on-message-stop-commands-from-working
-    
+
+
 @bot.command(aliases=['e'])
-async def echo(ctx: commands.Context, *, arg: str):
+async def echo(ctx: Ctx, *, arg: str):
     await ctx.send(arg)
 
-# @bot.command(aliases=['t2s'])
-# async def text2score(ctx: commands.Context, *, arg: str):
-    # t2s_m.Lily(ctx, arg)
+
+@bot.command(aliases=['t2s'])
+async def text2score(ctx: Ctx, *, arg: str):
+    t2s_m.lily(ctx, arg)
+
 
 @bot.event
 async def on_reaction_add(reaction: dc.reaction, user: dc.User):
     if user.bot: return
 
-bot.run(os.getenv("TOKEN2"))
+
+bot.run(os.getenv("TOKEN"))
