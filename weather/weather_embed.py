8622cb0cb4a33f3e73650c713dcb304911378025
DeemoHarlos <cces4415@gmail.com>
2021-08-17 19:05:25 +0800

    last last

--- weather/weather_embed.py
+++ weather/weather_embed.py
@@ -13,7 +13,7 @@ async def weather_explane(ctx: Ctx):
         name="資料從這邊整理來ㄉ(點我)", url="https://opendata.cwb.gov.tw/dataset/forecast/F-B0053-033")
     embed.set_thumbnail(
         url="https://img.cdn.nimg.jp/s/nicovideo/thumbnails/38135262/38135262.98063237.original/r1280x720l?key=4a095172beb00cb26112965018095abb2aa38b8b704a45b0029a39b4336a4d6f")
-    embed.add_field(name="圖表", value="!weather a 小風口停車場\n!weather b 鳶峰停車場\n!weather c 台大梅峰實驗農場\n!weather d 新中橫塔塔加停車場\n!weather e 武陵農場\n!weather f 大雪山森林遊樂區\n!weather g 陽明山小油坑\n!weather h 陽明山擎天崗\n", inline=True)
+    embed.add_field(name="圖表", value="!D weather a 小風口停車場\n!D weather b 鳶峰停車場\n!D weather c 台大梅峰實驗農場\n!D weather d 新中橫塔塔加停車場\n!D weather e 武陵農場\n!D weather f 大雪山森林遊樂區\n!D weather g 陽明山小油坑\n!D weather h 陽明山擎天崗\n", inline=True)
     embed.add_field(name="表格", value="!plot", inline=True)
     await ctx.send(embed=embed)
     
